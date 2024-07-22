093cbf51b771e8af10415a0bbebe3fdf0c2ae794
Anirudh Gupta <146579014+AnirudhG07@users.noreply.github.com>
2024-07-22 11:34:35 +0530

    gmail.py fixus

--- ntfyme/utils/mail/gmail.py
+++ ntfyme/utils/mail/gmail.py
@@ -8,25 +8,23 @@ def send_gmail(result):
     config_path = os.path.join(package_root, "config.toml")
 
     with open(config_path, "r") as file:
+        details = toml.load(file)
 
-        data = toml.load(file)
-
-    if data["mail"]["mail_id"].endswith("@gmail.com"):
+    mail_id = details["mail"]["mail_id"]
+    password = details["mail"]["password"]
+    
+    if mail_id.endswith("@gmail.com"):
         EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
         EMAIL_USE_TLS = True
-
         EMAIL_HOST = "smtp.gmail.com"
-
         EMAIL_PORT = 587
 
-        EMAIL_HOST_USER = data["mail"]["mail_id"]
-
-        EMAIL_HOST_USER = RECIEVER_S_MAIL_ID = "vishnuteja2045@gmail.com"
-        EMAIL_HOST_PASSWORD = "zfouotflyarcibpr" #data["mail"]["password"]
+        EMAIL_HOST_USER = RECIEVER_S_MAIL_ID = mail_id
+        EMAIL_HOST_PASSWORD = password
         connection = smtplib.SMTP("smtp.gmail.com", port=587)
         connection.starttls()
         connection.login(user=EMAIL_HOST_USER, password=EMAIL_HOST_PASSWORD)
+        
         connection.sendmail(
             from_addr=EMAIL_HOST_USER,
             to_addrs=RECIEVER_S_MAIL_ID,
