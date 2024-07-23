9cca55aa4000aed73fbb751619976686aeb8b608
Yash Kamble <142909905+YasKam24@users.noreply.github.com>
2024-07-24 01:42:23 +0530

    live capture configure done

--- ntfyme/cmd/live_capture.py
+++ ntfyme/cmd/live_capture.py
@@ -1,55 +1,36 @@
-import os
-import selectors
+
 import subprocess
 import sys
 import threading
-
+import os
 
 def read_output(pipe, output_list, print_func):
     """Read lines or characters from a pipe and print them, storing them in a list."""
-    selector = selectors.DefaultSelector()
-    selector.register(pipe, selectors.EVENT_READ)
     while True:
-        for key, _ in selector.select(timeout=1):
-            data = key.fileobj.read(1)  # Read one character at a time
-            if not data:
-                selector.unregister(pipe)
-                return
-            output_list.append(data)
-            print_func(data, end="", flush=True)
-
+        data = pipe.read(1)  # Read one character at a time
+        if not data:
+            break
+        output_list.append(data)
+        print_func(data, end='', flush=True)
 
 def capture(cmd):
+    # Set the PYTHONUNBUFFERED environment variable to force unbuffered output
     env = os.environ.copy()
     env["PYTHONUNBUFFERED"] = "1"
-    process = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-        text=True,
-        bufsize=0,
-        env=env,
-    )
+
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True, bufsize=0, env=env)
     output, error = [], []
-
-    stdout_thread = threading.Thread(
-        target=read_output, args=(process.stdout, output, print)
-    )
-    stderr_thread = threading.Thread(
-        target=read_output,
-        args=(
-            process.stderr,
-            error,
-            lambda x, **kwargs: print(x, file=sys.stderr, **kwargs),
-        ),
-    )
-
+    
+    stdout_thread = threading.Thread(target=read_output, args=(process.stdout, output, print))
+    stderr_thread = threading.Thread(target=read_output, args=(process.stderr, error, lambda x, **kwargs: print(x, file=sys.stderr, **kwargs)))
+    
     stdout_thread.start()
     stderr_thread.start()
-
+    
     stdout_thread.join()
     stderr_thread.join()
+    
+    process.wait()
 
     process.wait()
 
